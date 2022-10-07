4cb5ab72b67330c5d2a113307e7b189557bcc3ed
Shivani715 <shivani.patel@doctolib.com>
2022-10-07 16:02:51 +0200

    Basic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [image, setImage] = useState(1);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="thumbnailwrapper">
+        <div className="thumbnail" onClick={() => setImage(1)}>1</div>
+        <div className="thumbnail" onClick={() => setImage(2)}>2</div>
+        <div className="thumbnail" onClick={() => setImage(3)}>3</div>
+        <div className="thumbnail" onClick={() => setImage(4)}>4</div>
+      </div>
+      {image}
     </div>
   );
 }
