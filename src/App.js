a6d636ef2916e45469432b499f809b08a0045a78
Shivani715 <shivani.patel@doctolib.com>
2022-10-13 14:02:31 +0200

    addded code to color

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import { useState } from "react";
 
 function App() {
   const [image, setImage] = useState(1);
+  const [color, setColor] = useState("white")
 
   return (
     <div className="App">
-      <div className="thumbnailwrapper">
+      <div className="thumbnailwrapper" style={{backgroundColor:color}}>
         <div className="thumbnail" onClick={() => setImage(1)}>1</div>
         <div className="thumbnail" onClick={() => setImage(2)}>2</div>
         <div className="thumbnail" onClick={() => setImage(3)}>3</div>
         <div className="thumbnail" onClick={() => setImage(4)}>4</div>
       </div>
       {image}
+      <div onDoubleClick ={() => setColor("green")}>change color</div>
     </div>
   );
 }
