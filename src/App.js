0e2c67ca6f7e31c83803333ffa0fb6fb0ee418b3
Shivani715 <shivani.patel@doctolib.com>
2022-10-27 14:11:35 +0200

    Code to change the images on clicking

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import flower1 from "./IMAGES/flower1.png";
+import Flower1 from "./IMAGES/flower1.png";
+import Flower2 from "./IMAGES/Flower2.png";
+import Flower3 from "./IMAGES/Flower3.png";
+import Flower4 from "./IMAGES/flower4.png";
 import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [image, setImage] = useState(1);
+  const [image, setImage] = useState(Flower1);
 
   return (
     <div className="App">
       <div className="thumbnailwrapper">
-        <div className="thumbnail" onClick={() => setImage(1)}>
-          1
+        <div className="thumbnail" onClick={() => setImage(Flower1)}>
+          <img src={Flower1} width={100} />
         </div>
-        <div className="thumbnail" onClick={() => setImage(2)}>
-          2
+        <div className="thumbnail" onClick={() => setImage(Flower2)}>
+          <img src={Flower2} width={100} />
         </div>
-        <div className="thumbnail" onClick={() => setImage(3)}>
-          3
+        <div className="thumbnail" onClick={() => setImage(Flower3)}>
+          <img src={Flower3} width={100} />
         </div>
-        <div className="thumbnail" onClick={() => setImage(4)}>
-          4
+        <div className="thumbnail" onClick={() => setImage(Flower4)}>
+          <img src={Flower4} width={100} />
         </div>
       </div>
-      <img src={flower1} width={500} />
+      <img src={image} width={500} />
     </div>
   );
 }
