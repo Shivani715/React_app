2384de00ab4bffe972a597b646fefe67b77053c1
Shivani715 <shivani.patel@doctolib.com>
2022-10-27 13:58:20 +0200

    Added images

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import logo from "./logo.svg";
+import flower1 from "./IMAGES/flower1.png";
 import "./App.css";
 import { useState } from "react";
 
 function App() {
   const [image, setImage] = useState(1);
-  const [color, setColor] = useState("white")
 
   return (
     <div className="App">
-      <div className="thumbnailwrapper" style={{backgroundColor:color}}>
-        <div className="thumbnail" onClick={() => setImage(1)}>1</div>
-        <div className="thumbnail" onClick={() => setImage(2)}>2</div>
-        <div className="thumbnail" onClick={() => setImage(3)}>3</div>
-        <div className="thumbnail" onClick={() => setImage(4)}>4</div>
+      <div className="thumbnailwrapper">
+        <div className="thumbnail" onClick={() => setImage(1)}>
+          1
+        </div>
+        <div className="thumbnail" onClick={() => setImage(2)}>
+          2
+        </div>
+        <div className="thumbnail" onClick={() => setImage(3)}>
+          3
+        </div>
+        <div className="thumbnail" onClick={() => setImage(4)}>
+          4
+        </div>
       </div>
-      {image}
-      <div onDoubleClick ={() => setColor("green")}>change color</div>
+      <img src={flower1} width={500} />
     </div>
   );
 }
